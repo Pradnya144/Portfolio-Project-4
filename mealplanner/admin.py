4b06e193cfde478d84eea2a282debd6001b43bc3
Pradnya Bhagwat <pradnya144@gmail.com>
2022-08-27 19:35:13 +0000

    Add basic create basic boilerplate for base.html

--- mealplanner/admin.py
+++ mealplanner/admin.py
@@ -4,6 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipes)
 class RecipesAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ('title',)
