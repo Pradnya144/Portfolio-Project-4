fabaece3dad510f806a24e18a4ad9d6c4dee0755
Pradnya Bhagwat <pradnya144@gmail.com>
2022-08-28 14:05:42 +0000

    Add initial details in views.py

--- mealplanner/views.py
+++ mealplanner/views.py
@@ -1,1 +1,16 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Recipes, Comments, MealPlan
+
+class Home(generic.TemplateView):
+    template_name = "index.html"
+
+    
+class RecipeList(generic.ListView):
+    model = Recipes
+    queryset = Recipe.objects.filter(ststus=1).order_by('-created_on')
+    template_name = 'browse_recipes.html'
+    paginate_by = 8
+
+
+
